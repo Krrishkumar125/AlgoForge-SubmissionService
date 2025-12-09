6b59204218e3145f4ad694590355e467b69e8c5f
Krrishkumar125 <krrishkumar2028@gmail.com>
2025-12-09 15:00:29 +0530

    Fix: Added connect DB call

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const app = require("./app");
+const connectToDB = require("./config/dbConfig");
 
 const fastify = require("fastify")({ logger: true });
 
@@ -6,10 +7,11 @@ const serverConfig = require("./config/serverConfig");
 
 fastify.register(app);
 
-fastify.listen({ port: serverConfig.PORT }, (err) => {
+fastify.listen({ port: serverConfig.PORT }, async (err) => {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
   }
+  await connectToDB();
   console.log(`Server up at port ${serverConfig.PORT} and running in ${serverConfig.NODE_ENV} mode`);
 });
