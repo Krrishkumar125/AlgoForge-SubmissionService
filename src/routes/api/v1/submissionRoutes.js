cd92d209808a4f576272e9a6e84f9fbe8b751181
Krrishkumar125 <krrishkumar2028@gmail.com>
2025-12-11 15:26:43 +0530

    Fix: Added ping route for checking health of service

--- src/routes/api/v1/submissionRoutes.js
+++ src/routes/api/v1/submissionRoutes.js
@@ -2,6 +2,9 @@ const submissionController = require("../../../controllers/submissionController"
 
 async function submissionRoutes(fastify, options) {
   fastify.post("/", submissionController.createSubmission.bind(fastify));
+  fastify.get("/ping", (request, reply) => {
+    return reply.send("Submission Service is up and running");
+  });
 }
 
 module.exports = submissionRoutes;
